3f6084328fd1a26372a0a2731ee83eb345591714
Hien Le <lnthien@tma.com.vn>
2019-05-09 17:47:38 +0700

    Updates

---
+++ precache-manifest.9b0dab129a33b6a8e364dfba1676d0a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf0e0ea3b1d54a1dd071d9469dbe44a",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "f8fc0a5967fdc5ebcdf6",
+    "url": "/test-react/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "453ded4a47a1a1a2e733",
+    "url": "/test-react/static/js/2.774c5952.chunk.js"
+  },
+  {
+    "revision": "f8fc0a5967fdc5ebcdf6",
+    "url": "/test-react/static/js/main.be865eaf.chunk.js"
+  },
+  {
+    "revision": "31d3ffe18f2cb45b7bd6",
+    "url": "/test-react/static/js/runtime~main.bea1702e.js"
+  }
+]);
